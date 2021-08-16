4ba4b9c4bfd148a1500c05307633590e39413ab8
Peterson-moura <peterson.moura@outlook.com.br>
2021-08-16 12:31:55 -0300

    _editado_

--- Projeto_DWII/dwii-projeto-peterson/src/pages/Homep.vue
+++ Projeto_DWII/dwii-projeto-peterson/src/pages/Homep.vue
@@ -2,22 +2,20 @@
   <div class="home">
     <Headerp></Headerp>
     <Agendanew></Agendanew>
-    <detalheproduto></detalheproduto>
+    
   </div>
 </template>
 
 <script>
 import Agendanew from '../components/Agendanew.vue';
 import Headerp from "../components/Headerp.vue";
-import detalheproduto from '../components/detalheproduto.vue';
+
 
 export default {
   name: "Home",
   components: {
     Headerp,
-    Agendanew,
-    detalheproduto,
-   
+    Agendanew,   
     },
 };
 </script>
--- Projeto_DWII/dwii-projeto-peterson/src/routes.js
+++ Projeto_DWII/dwii-projeto-peterson/src/routes.js
@@ -1,13 +1,12 @@
 import VueRouter from "vue-router";
 import Homep from "./pages/Homep.vue";
-import detalheproduto from "./pages/detalheproduto.vue";
+
 
 const routes = [
     {
         path: "/", component: Homep
-    },{
-        path: "/produto",component: detalheproduto
-    }
+    },
+   
 ]
 
 const router  = new VueRouter({
